65012e0d45e976a6a9afb524951c78c63769a357
Savitha Gollamudi <savitha.gollamudi@gmail.com>
2021-01-31 17:25:49 +0530

    Time: 0 ms (100.00%), Space: 38.7 MB (92.12%) - LeetHub

--- maximum-subarray/maximum-subarray.java
+++ maximum-subarray/maximum-subarray.java
@@ -2,13 +2,13 @@ class Solution {
     public int maxSubArray(int[] nums) {
         if(nums.length == 0) return 0;
         //kadane's algorithm
-        int res = nums[0];
+        int maxSoFar = nums[0];
         int maxEnding = nums[0];
         
         for(int i = 1; i < nums.length; i++){
             maxEnding = Math.max(maxEnding + nums[i] , nums[i]);
-            res = Math.max(maxEnding,res);
+            maxSoFar = Math.max(maxEnding,maxSoFar);
         }
-        return res;
+        return maxSoFar;
     }
 }
