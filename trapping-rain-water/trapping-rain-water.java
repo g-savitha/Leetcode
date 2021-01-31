6466335eccea8048ddf5fc955d4658aa201581f5
Savitha Gollamudi <savitha.gollamudi@gmail.com>
2021-01-31 15:41:05 +0530

    Time: 1 ms (85.07%), Space: 39.8 MB (14.61%) - LeetHub

--- trapping-rain-water/trapping-rain-water.java
+++ trapping-rain-water/trapping-rain-water.java
@@ -7,23 +7,58 @@ class Solution {
             return 0;
         }
         
-        int[] lmax = new int[n];
-        int[] rmax = new int[n];
+        //2 pointer approach
         
-        lmax[0] = height[0];
-        rmax[n-1] = height[n-1];
-        //calculate max height of building from left and store
-        for(int i = 1; i < n; i++){
-            lmax[i] = Math.max(height[i],lmax[i-1]);
+        int lB = height[0];
+        int rB = height[n-1];
+        
+        int res = 0;
+        
+        int left = 0, right = n-1;
+        
+        while(left < right){
+            if(height[left]< height[right]){
+                
+                if(height[left]>=lB){
+                    lB = height[left];
+                }
+                else{
+                 res += lB - height[left];
+                }
+                left++;
+            }
+            else{
+                if(height[right]>=rB){
+                    rB = height[right];
+                }
+                else{
+                    res += rB - height[right];
+                }
+                right--;
+            }
         }
-        //calculate the max height of building from right and store
-        for(int i = n-2; i>=0; i--){
-            rmax[i] = Math.max(height[i],rmax[i+1]);
-        }
-        //calculate collected water
-        for(int i =1; i < n; i++){
-            water += (Math.min(lmax[i],rmax[i]) - height[i]);
-        }
-        return water;
+        return res;
+        
+        //TC : O(n), SC: O(1)
+        
+        
+//         int[] lmax = new int[n];
+//         int[] rmax = new int[n];
+        
+//         lmax[0] = height[0];
+//         rmax[n-1] = height[n-1];
+//         //calculate max height of building from left and store
+//         for(int i = 1; i < n; i++){
+//             lmax[i] = Math.max(height[i],lmax[i-1]);
+//         }
+//         //calculate the max height of building from right and store
+//         for(int i = n-2; i>=0; i--){
+//             rmax[i] = Math.max(height[i],rmax[i+1]);
+//         }
+//         //calculate collected water
+//         for(int i =1; i < n; i++){
+//             water += (Math.min(lmax[i],rmax[i]) - height[i]);
+//         }
+//         return water;
     }
-}
+} //TC: O(n) , SC : O(n)
