bd25bd17a2cf7bae202df0502bd6e809b3232a94
Savitha Gollamudi <gsavitha@protonmail.com>
2021-07-21 15:04:11 +0530

    Added solution - LeetHub

---
+++ 4. Square root of a number - GFG/4.-square-root-of-a-number.java
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+import java.util.Scanner;
+
+class SquartRoot
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			long a = sc.nextInt();
+			Solution obj = new Solution();
+			System.out.println(obj.floorSqrt(a));
+		t--;
+		}
+	}
+}// } Driver Code Ends
+
+
+/*You are required to complete
+this function*/
+
+// Function to find square root
+// x: element to find square root
+class Solution
+{
+     long floorSqrt(long x)
+	 {
+		// Your code here
+		long ans = 0;
+		long low = 1, high = x;
+		while(low <= high){
+		    long mid = (low+high) / 2;
+		    long msq = mid * mid;
+
+		    if(msq == x){
+		        return mid;
+		    }
+		    else if(msq > x) {
+		        high = mid - 1;
+		    }
+		    else {low = mid + 1;
+		        ans = mid;
+		    }
+		    }
+		    return ans;
+		}
+	 }
